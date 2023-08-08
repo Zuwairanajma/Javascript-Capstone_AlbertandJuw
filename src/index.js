92baa209975ccc93eea59d3ef4c4afa9fb1b7c76
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-08-08 15:14:29 +0100

    Add likes icon and counter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // index.js
 
 import './style.css';
-import getData from './modules/displayList.js';
+// import getData from './modules/displayList.js';
+
+// getData();
+
+import { displayShows } from "./modules/displayList.js";
+import { handleLikes } from "./modules/displayLikes.js";
+
+async function getData() {
+  const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
+  const data = await response.json();
+  displayShows(data);
+  handleLikes();
+}
 
 getData();
--- src/modules/displayList.js
+++ src/modules/displayList.js
@@ -1,59 +1,27 @@
-// export async function getData() {
-//     const response = await fetch("https://api.tvmaze.com/search/shows?q=girls");
-//     const data = await response.json();
-//     const showList = document.querySelector(".show-home");
-// console.log(data);
-//     for (let i = 0; i < 15; i += 1) {
-//         if (data[i] && data[i].show) {
-//         const showData = data[i].show;
-//         }
-//         const list = document.createElement("div");
-//         list.className = "shows";
-
-//         const div = document.createElement("div");
-//         div.className = "div-img";
-//         list.appendChild(div);
-
-//         const img = document.createElement("img");
-//         img.src = showData.image ? showData.image.medium : 'placeholder.jpg';
-//         div.appendChild(img);
-
-//         // Create and add title
-//         const title = document.createElement("h3");
-//         title.textContent = showData.name || "Title Not Available";
-//         list.appendChild(title);
-//         showList.appendChild(list);
-//     }
-// }
-async function getData() {
-  const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
-  const data = await response.json();
-  const showList = document.querySelector('.show-home');
-
-  for (let i = 0; i < 15; i += 1) {
-    let showData = null; // Declare the variable in a higher scope
-
-    if (data[i] && data[i].show) {
-      showData = data[i].show; // Assign the value if condition is met
+export async function displayShows(data) {
+    const showList = document.querySelector('.show-home');
+  
+    for (let i = 0; i < 15; i += 1) {
+      const showData = data[i] && data[i].show ? data[i].show : null;
+  
+      const list = document.createElement('div');
+      list.className = 'shows';
+  
+      const div = document.createElement('div');
+      div.className = 'div-img';
+      list.appendChild(div);
+  
+      const img = document.createElement('img');
+      img.src = showData ? showData.image.medium : ''; // No default image
+      div.appendChild(img);
+  
+      if (showData && showData.name) {
+        const title = document.createElement('h3');
+        title.textContent = showData.name;
+        list.appendChild(title);
+  
+        showList.appendChild(list);
+      }
     }
-
-    const list = document.createElement('div');
-    list.className = 'shows';
-
-    const div = document.createElement('div');
-    div.className = 'div-img';
-    list.appendChild(div);
-
-    const img = document.createElement('img');
-    img.src = showData ? showData.image.medium : './assets/placeholder.png';
-    div.appendChild(img);
-
-    // Create and add title
-    const title = document.createElement('h3');
-    title.textContent = showData ? showData.name : 'Title Not Available';
-    list.appendChild(title);
-
-    showList.appendChild(list);
   }
-}
-export default getData;
+  
