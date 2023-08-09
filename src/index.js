f02f7e847fd6cf95b1af3b8df97af9d4310f9c32
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-08-09 07:28:51 +0100

    Add involvement Api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // index.js
 
 import './style.css';
-import displayShows from './modules/displayList.js';
-import handleLikes from './modules/displayLikes.js';
+// import displayShows from './modules/displayList.js';
+import getData from './modules/displayLikes.js';
 
-async function getData() {
-  const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
-  const data = await response.json();
-  displayShows(data);
-  handleLikes();
-}
+// async function getData() {
+//   const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
+//   const data = await response.json();
+//   displayShows(data);
+//   handleLikes();
+// }
 
 getData();
--- src/modules/displayList.js
+++ src/modules/displayList.js
@@ -1,27 +1,27 @@
-async function displayShows(data) {
-  const showList = document.querySelector('.show-home');
+// async function displayShows(data) {
+//   const showList = document.querySelector('.show-home');
 
-  for (let i = 0; i < 15; i += 1) {
-    const showData = data[i] && data[i].show ? data[i].show : null;
+//   for (let i = 0; i < 15; i += 1) {
+//     const showData = data[i] && data[i].show ? data[i].show : null;
 
-    const list = document.createElement('div');
-    list.className = 'shows';
+//     const list = document.createElement('div');
+//     list.className = 'shows';
 
-    const div = document.createElement('div');
-    div.className = 'div-img';
-    list.appendChild(div);
+//     const div = document.createElement('div');
+//     div.className = 'div-img';
+//     list.appendChild(div);
 
-    const img = document.createElement('img');
-    img.src = showData ? showData.image.medium : ''; // No default image
-    div.appendChild(img);
+//     const img = document.createElement('img');
+//     img.src = showData ? showData.image.medium : ''; // No default image
+//     div.appendChild(img);
 
-    if (showData && showData.name) {
-      const title = document.createElement('h3');
-      title.textContent = showData.name;
-      list.appendChild(title);
+//     if (showData && showData.name) {
+//       const title = document.createElement('h3');
+//       title.textContent = showData.name;
+//       list.appendChild(title);
 
-      showList.appendChild(list);
-    }
-  }
-}
-export default displayShows;
+//       showList.appendChild(list);
+//     }
+//   }
+// }
+// export default displayShows;
