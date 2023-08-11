93951d95e3637ef7c034f6ac3ae744b15b45a54d
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-08-11 09:20:30 +0100

    Add list, likes and meals count functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-// index.js
-
 import './style.css';
-import { getData, handleLikes } from './modules/displayList.js';
-// import handleLikes from './modules/displayLikes.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  getData();
-  handleLikes();
-});
+import getData from './modules/mealDisplay';
+
+getData();
